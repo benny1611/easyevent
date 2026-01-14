dc0a56acbfe73398fe9127d38a5a86229bdcfd16
benny1611 <benny_1611@protonmail.com>
2026-01-14 21:25:44 +0100

    added navbar + translation + installed svg stuff

---
+++ easyevent_frontend/easyevent/src/i18n/en.ts
@@ -0,0 +1,12 @@
+const en = {
+    appName: "EasyEvent",
+
+    nav: {
+        login: "Login",
+        home: "Home",
+        create: "Create",
+        register: "Register"
+    },
+};
+
+export default en;
---
+++ easyevent_frontend/easyevent/src/i18n/i18nContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from "react";
+import en from "./en";
+
+type Language = "en";
+
+const translations = {en};
+
+const I18nContext = createContext({
+    text: translations.en,
+    setLanguage: (_: Language) => {},
+});
+
+export const I18nProvider: React.FC<{children: React.ReactNode }> = ({ children }) => {
+    const [language, setLanguage] = useState<Language>("en");
+
+    return(
+        <I18nContext.Provider
+            value={{
+                text: translations[language],
+                setLanguage,
+            }}
+        >
+            {children}
+        </I18nContext.Provider>
+    );
+}
+
+export const useI18n = () => useContext(I18nContext);
