1e0dc52dc55396e272602cefecad51d769d2f668
benny1611 <benny_1611@protonmail.com>
2026-01-21 21:24:01 +0100

    added AuthContext, now we can start to implement the login page

---
+++ easyevent_frontend/easyevent/src/auth/AuthContext.tsx
@@ -0,0 +1,91 @@
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import type { JwtPayload } from "./JwtPayload";
+
+
+type AuthContextType = {
+    token: string | null;
+    roles: string[];
+    isAuthenticated: boolean;
+    hasRole: (role: string) => boolean;
+    login: (token: string) => void;
+    logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+const STORAGE_KEY = "auth_token";
+
+function decodeJwt(token: string): JwtPayload {
+    const payload = token.split('.')[1];
+    return JSON.parse(atob(payload));
+}
+
+export function AuthProvider({children}: {children: React.ReactNode}) {
+    const [token, setToken] = useState<string|null>(() => localStorage.getItem(STORAGE_KEY));
+    const [payload, setPayload] = useState<JwtPayload | null>(null);
+
+    useEffect(() => {
+        if(!token) {
+            setPayload(null);
+            return;
+        }
+        try {
+            const decoded = decodeJwt(token);
+            setPayload(decoded);
+        } catch {
+            logout();
+        }
+    }, [token]);
+
+    useEffect(() => {
+        if(!payload) {
+            return;
+        }
+
+        const expiresAtMs = payload.exp * 1000;
+        const timeout = expiresAtMs - Date.now();
+
+        if(timeout <= 0) {
+            logout();
+            return;
+        }
+
+        const timer = setTimeout(logout, timeout);
+        return () => clearTimeout(timer);
+    }, [payload]);
+
+    const login = (jwt: string) => {
+        localStorage.setItem(STORAGE_KEY, jwt);
+        setToken(jwt);
+    }
+
+    const logout = () => {
+        localStorage.removeItem(STORAGE_KEY);
+        setToken(null);
+        setPayload(null);
+    };
+
+    const value = useMemo<AuthContextType>(() => ({
+        token,
+        isAuthenticated: !!token,
+        userEmail: payload?.sub ?? null,
+        roles: payload?.roles ?? [],
+        login,
+        logout,
+        hasRole: (role: string) => payload?.roles.includes(role) ?? false,
+    }), [token, payload]);
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export function useAuth() {
+    const ctx = useContext(AuthContext);
+    if(!ctx) {
+        throw new Error("useAuth must be used inside AuthProvider");
+    }
+    return ctx;
+}
