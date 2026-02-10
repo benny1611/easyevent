7a014855c1edc911673a55079031ed23d9371938
benny1611 <benny_1611@protonmail.com>
2026-02-10 20:44:57 +0100

    added email validation to the DTOs

--- easyevent_backend/src/main/java/com/benny1611/easyevent/dto/CreateUserRequest.java
+++ easyevent_backend/src/main/java/com/benny1611/easyevent/dto/CreateUserRequest.java
@@ -1,5 +1,6 @@
 package com.benny1611.easyevent.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -11,6 +12,7 @@ public class CreateUserRequest {
     @NotBlank(message = "name must not be empty")
     private String name;
 
+    @Email
     @NotBlank(message = "email must not be empty")
     private String email;
 
--- easyevent_backend/src/main/java/com/benny1611/easyevent/dto/EventRegistrationRequest.java
+++ easyevent_backend/src/main/java/com/benny1611/easyevent/dto/EventRegistrationRequest.java
@@ -1,5 +1,6 @@
 package com.benny1611.easyevent.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -10,6 +11,7 @@ public class EventRegistrationRequest {
     @NotBlank
     private String name;
 
+    @Email
     @NotBlank
     private String email;
 
